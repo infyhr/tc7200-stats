324cfea663b23a08c83cbe0b45d976870b783f2f
infyhr <infy@cybershade.org>
2017-02-26 14:28:16 +0100

    Added downstream pattern match

--- js/index.js
+++ js/index.js
@@ -18,6 +18,35 @@ function getRandomArbitrary(min, max) {
 }
 
 /*
+Extracts data from the HTTP response and returns it, just some simple regex stuff
+*/
+function extractData(data) {
+    // Downstream
+    const re_dsChannels = /\d<\/td><td align="right">(\d{1,2})<\/td>/gi
+    const re_dsSnr = /<td align="right">(\d{1,2}\.\d) dB<\/td>/gi
+    const re_dsPwr = /<td align="right">(\s|-)?([1-9]{1,2}\.\d) dBmV<\/td><t/gi
+
+    // Upstream
+    const re_usChannels = /\d<\/td><td align="right">\d{1,2}<\/td>/gi
+    const re_usPower = /<td align="right">?(\d{1,2}\.\d) dBmV<\/td><\//gi
+
+    // Execute downstream regex matching
+    let ds_channels = re_dsChannels.exec(data);
+    let ds_snr = re_dsSnr.exec(data);
+    let ds_pwr = re_dsPwr.exec(data);
+    // Loop through all the results
+    while(ds_snr != null) { // Will be the same for all three
+        console.log(ds_channels[1]);
+        console.log(ds_snr[1]);
+        console.log(ds_pwr[2]);
+
+        ds_channels = re_dsChannels.exec(data);
+        ds_snr = re_dsSnr.exec(data);
+        ds_pwr = re_dsPwr.exec(data);
+    }
+}
+
+/*
 This will be called periodically to load new stats, so called short-polling
 Must be exported because we'd like to access it outside, async events could have also been used
 good read: http://electron.rocks/different-ways-to-communicate-between-main-and-renderer-process/
@@ -25,9 +54,9 @@ good read: http://electron.rocks/different-ways-to-communicate-between-main-and-
 Also, *must* utilize a callback to return values from an asynchronous request, obviously.
 */
 exports.getData = function(callback) {
-    data = [123]; // Will hold all pairs
+    ret = []; // Will hold all pairs (to be returned)
 
-    //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+    // Construct the header
     var auth = 'Basic ' + new Buffer(':admin').toString('base64');
     var options = {
         host: '192.168.0.1',
@@ -37,17 +66,18 @@ exports.getData = function(callback) {
     };
 
     res = function(response) {
-        var str = '';
+        var data = '';
 
-        //another chunk of data has been recieved, so append it to `str`
+        // Another chunk of data has been recieved, so append it to `data`
         response.on('data', function (chunk) {
-            str += chunk;
+            data += chunk;
         });
 
-        //the whole response has been recieved, so we just print it out here
+        // The whole response has been recieved
         response.on('end', function () {
-            console.log('end!!!');
-            data = [
+            ret = extractData(data);
+            //console.log(test);
+            /*data = [
                 [
                 getRandomArbitrary(20, 40),
                 40.8,
@@ -61,15 +91,14 @@ exports.getData = function(callback) {
                 43.0
                 ]
             ]
-            //console.log(str);*/
-            callback(data);
+            //console.log(data);
+            callback(data);*/
         });
 
         response.on('error', function(e) {
             console.log(e);
         });
     }
-    http.request(options, res).end();
 
-    //return data;
+    http.request(options, res).end();
 }
